e0c1487394f34fb930c864e141244deaa0e23ee3
martin <mperezguevara@gmail.com>
2015-07-15 19:01:35 +0200

    finished new install documentation web component inside the page example

--- _static/js/tabs.js
+++ _static/js/tabs.js
@@ -1,3 +1,16 @@
+var OSName=2;
+if (navigator.appVersion.indexOf("Win")!=-1) OSName=1;
+if (navigator.appVersion.indexOf("Mac")!=-1) OSName=2;
+if (navigator.appVersion.indexOf("X11")!=-1) OSName=3;
+if (navigator.appVersion.indexOf("Linux")!=-1) OSName=3;
+
+$(document).ready(function(){
+    $("ul#tabs li.active").removeClass("active");
+    $("ul#tab li.active").removeClass("active");
+    $("ul#tabs li:nth-child("+OSName+")").addClass("active");
+    $("ul#tab li:nth-child("+OSName+")").addClass("active");
+});
+
 $(document).ready(function(){
     $("ul#tabs li").click(function(e){
         if (!$(this).hasClass("active")) {
